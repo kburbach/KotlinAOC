a7132c64d0ee52e1bad2dcb9ff68e83888da1cb8
Kevin Burbach <kevin.burbach@gmail.com>
2023-11-29 13:30:21 -0700

    Cleaned up toString in sealed class

--- src/day10/Pixel.kt
+++ src/day10/Pixel.kt
@@ -1,15 +1,8 @@
 package day10
 
-sealed class Pixel(open val s: String) {
-    override fun toString() = s // so this literally does nothing?
+sealed class Pixel(private val s: String) {
+    final override fun toString() = s
 }
 
-data class LitPixel(override val s: String = "#") : Pixel(s) {
-
-    //without this (useless?) override, LitPixel doesn't inherit Pixel.toString()...
-    override fun toString() = super.toString()
-}
-
-data class DarkPixel(override val s: String = ".") : Pixel(s) {
-    override fun toString() = super.toString()
-}
+class LitPixel : Pixel("#")
+class DarkPixel : Pixel(".")
