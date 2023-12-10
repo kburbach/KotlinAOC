4e457dfc78147ec3178d54d1310f2020368acbee
Kevin Burbach <kevin.burbach@gmail.com>
2023-12-10 00:10:31 -0700

    cleanup stuff from day11

--- src/_2022/day11/MonkeyDefinitions.kt
+++ src/_2022/day11/MonkeyDefinitions.kt
@@ -5,7 +5,7 @@ import Monkey
 fun getMonkeys(): List<Monkey> {
 
     val monkey0 = Monkey(
-        items = mutableListOf<Long>(50, 70, 89, 75, 66, 66),
+        items = mutableListOf(50, 70, 89, 75, 66, 66),
         operation = { it.times(5) },
         test = 2,
         trueMonkey = 2,
@@ -13,7 +13,7 @@ fun getMonkeys(): List<Monkey> {
     )
 
     val monkey1 = Monkey(
-        items = mutableListOf<Long>(85),
+        items = mutableListOf(85),
         operation = { it.times(it) },
         test = 7,
         trueMonkey = 3,
@@ -21,7 +21,7 @@ fun getMonkeys(): List<Monkey> {
     )
 
     val monkey2 = Monkey(
-        items = mutableListOf<Long>(66, 51, 71, 76, 58, 55, 58, 60),
+        items = mutableListOf(66, 51, 71, 76, 58, 55, 58, 60),
         operation = { it.plus(1) },
         test = 13,
         trueMonkey = 1,
@@ -29,7 +29,7 @@ fun getMonkeys(): List<Monkey> {
     )
 
     val monkey3 = Monkey(
-        items = mutableListOf<Long>(79, 52, 55, 51),
+        items = mutableListOf(79, 52, 55, 51),
         operation = { it.plus(6) },
         test = 3,
         trueMonkey = 6,
@@ -37,7 +37,7 @@ fun getMonkeys(): List<Monkey> {
     )
 
     val monkey4 = Monkey(
-        items = mutableListOf<Long>(69, 92),
+        items = mutableListOf(69, 92),
         operation = { it.times(17) },
         test = 19,
         trueMonkey = 7,
@@ -45,7 +45,7 @@ fun getMonkeys(): List<Monkey> {
     )
 
     val monkey5 = Monkey(
-        items = mutableListOf<Long>(71, 76, 73, 98, 67, 79, 99),
+        items = mutableListOf(71, 76, 73, 98, 67, 79, 99),
         operation = { it.plus(8) },
         test = 5,
         trueMonkey = 0,
@@ -53,7 +53,7 @@ fun getMonkeys(): List<Monkey> {
     )
 
     val monkey6 = Monkey(
-        items = mutableListOf<Long>(82, 76, 69, 69, 57),
+        items = mutableListOf(82, 76, 69, 69, 57),
         operation = { it.plus(7) },
         test = 11,
         trueMonkey = 7,
@@ -61,7 +61,7 @@ fun getMonkeys(): List<Monkey> {
     )
 
     val monkey7 = Monkey(
-        items = mutableListOf<Long>(65, 79, 86),
+        items = mutableListOf(65, 79, 86),
         operation = { it.plus(5) },
         test = 17,
         trueMonkey = 5,
@@ -82,7 +82,7 @@ fun getMonkeys(): List<Monkey> {
 
 fun getTestMonkeys(): List<Monkey> {
     val monkey0 = Monkey(
-        items = mutableListOf<Long>(79, 98),
+        items = mutableListOf(79, 98),
         operation = { it.times(19) },
         test = 23,
         trueMonkey = 2,
@@ -90,7 +90,7 @@ fun getTestMonkeys(): List<Monkey> {
     )
 
     val monkey1 = Monkey(
-        items = mutableListOf<Long>(54, 65, 75, 74).toMutableList(),
+        items = mutableListOf(54, 65, 75, 74),
         operation = { it.plus(6) },
         test = 19,
         trueMonkey = 2,
@@ -98,7 +98,7 @@ fun getTestMonkeys(): List<Monkey> {
     )
 
     val monkey2 = Monkey(
-        items = mutableListOf<Long>(79, 60, 97).toMutableList(),
+        items = mutableListOf(79, 60, 97),
         operation = { it.times(it) },
         test = 13,
         trueMonkey = 1,
@@ -106,7 +106,7 @@ fun getTestMonkeys(): List<Monkey> {
     )
 
     val monkey3 = Monkey(
-        items = mutableListOf<Long>(74).toMutableList(),
+        items = mutableListOf(74),
         operation = { it.plus(3) },
         test = 17,
         trueMonkey = 0,
